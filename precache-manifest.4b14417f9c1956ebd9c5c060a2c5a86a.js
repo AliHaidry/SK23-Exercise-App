0171b74f5980e90f5d40baab42d639c5379a521d
AliHaidry <alihaidry11@gmail.com>
2021-02-04 19:22:44 -0500

    Updates

---
+++ precache-manifest.4b14417f9c1956ebd9c5c060a2c5a86a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2fb73897d88b751a55e60f832978d0",
+    "url": "/SK23-Exercise-App/index.html"
+  },
+  {
+    "revision": "f60b977c13fa57e30729",
+    "url": "/SK23-Exercise-App/static/css/2.392f69cd.chunk.css"
+  },
+  {
+    "revision": "f60b977c13fa57e30729",
+    "url": "/SK23-Exercise-App/static/js/2.170e5280.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/SK23-Exercise-App/static/js/2.170e5280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e8e9f92969bc60b246",
+    "url": "/SK23-Exercise-App/static/js/main.30a4f264.chunk.js"
+  },
+  {
+    "revision": "3219902bf21c052ec0b3",
+    "url": "/SK23-Exercise-App/static/js/runtime-main.281bf86c.js"
+  }
+]);
